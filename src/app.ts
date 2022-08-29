c4ae23b731ee3ceebf1854abd860ed7b2c622a71
Yuri An <yuriandev@gmail.com>
2022-08-29 16:06:27 +0300

    feat: add simple js selectors and first event listener. Lesson 123 Interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -14,7 +17,29 @@ class ProjectInput {
       true
     );
     this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = "user-input";
+
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector("#description")
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")
+    );
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
