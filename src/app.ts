426af23e40b2e5edf65a7569fbee384efb5024b7
Yuri An <yuriandev@gmail.com>
2022-09-03 22:14:22 +0300

    feat: add first decorator, autobind

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+// autobind decorator
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -32,7 +50,7 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
-
+  // @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
