cee81aa67659d6116c10fdfc23e1d745508f9426
Yuri An <yuriandev@gmail.com>
2022-11-27 20:14:38 +0600

    feat: add fetching user inputs. Lesson 125.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Study project. Drag and drop.
+From the course "Understanding Typescript 2023 edition" by Maximilian Schwarzmüller.
+[The link](https://www.udemy.com/course/understanding-typescript/)
--- src/app.ts
+++ src/app.ts
@@ -50,14 +50,42 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
-  // @autobind
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input, try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
